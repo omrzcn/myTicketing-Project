65aa6352f5804cb53320c482f762eb5f279bdcb8
omrzcn <omrzcnn@gmail.com>
2024-04-11 18:33:49 +0300

    getting data from the database

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.cydeo.controller;
 
-import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +10,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+private final RoleService roleService;
 
+    public UserController(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
     @GetMapping("/create")
     public String createUser(Model model){
         model.addAttribute("user",new UserDTO());
-//        model.addAttribute("roles", find all roles from db); // we will get roles from database for "roles" for create.html line 104 with help of service package
+     model.addAttribute("roles", roleService.findAll()); // we will get roles from database for "roles" for create.html line 104 with help of service package
         // we will create interface for role in service package
 
 
